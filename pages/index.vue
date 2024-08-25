ce5a157fdefef603c8c20ce2b179d7b9897ebf52
Ali Guliyev <ali.guliyevoo7@gmail.com>
2024-08-25 11:46:30 +0300

    init commit, under construction

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Site under construction</h1>
+    <a href="https://aliguliyev-v2.netlify.app"
+      >Go to the latest version of portfolio</a
+    >
+  </div>
+</template>
+
+<style>
+* {
+  text-align: center;
+}
+</style>
