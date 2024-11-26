9268c46387b87147dd2aa6aae64abb35f99bd458
Ali-Guliyev <ali.guliyevoo7@gmail.com>
2024-11-26 14:54:13 +0300

    chore: add packages and deps deps: nuxt content, unocss, iconify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/styles/main.scss'],
-  modules: [
-    '@unocss/nuxt',
-  ],
+  modules: ['@unocss/nuxt', '@nuxt/content'],
+  content: {
+    documentDriven: true
+  },
+  components: {
+    global: true,
+    dirs: ['~/components']
+  },
+  plugins: ['~/plugins/iconify.ts'],
 })
