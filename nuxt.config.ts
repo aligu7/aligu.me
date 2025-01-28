9fd7e5ec5be76f9b4ff750dfe3b33425868fa67a
aligu7 <ali.guliyevoo7@gmail.com>
2025-01-28 20:24:48 +0300

    feat: integrate nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/styles/main.scss'],
-  modules: ['@unocss/nuxt', '@nuxt/content', '@nuxthub/core'],
+  modules: ['@unocss/nuxt', '@nuxt/content', '@nuxthub/core', '@nuxthq/studio'],
   content: {
     documentDriven: true,
+    preview: {
+      api: 'https://api.nuxt.studio',
+    },
   },
   components: {
     global: true,
