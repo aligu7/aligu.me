32f97699db98a8d435a817f220deea29de0f01c0
aligu7 <ali.guliyevoo7@gmail.com>
2025-01-31 23:59:24 +0300

    chore: add tags to individual blog post page

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -23,8 +23,12 @@ function formatDate(date) {
   <article v-if="post" class="mx-auto page">
     <BackButton path="/blog" class="mb-2" />
     <h1 class="text-4xl">{{ post.title }}</h1>
+    <ul class="list-none flex flex-row items-center gap-3 mt-2">
+      <li v-for="tag in post.tags" :key="tag">
+        <Tag :name="tag" />
+      </li>
+    </ul>
     <p class="text-gray-600 mt-2">{{ formatDate(post.date) }}</p>
-
     <ContentRenderer :value="post" class="blog post" />
     <BackButton path="/blog" class="mt-2" />
   </article>
