6a5b1cfe2e73fd2ada0040e0a3d4843e4f5522da
aligu7 <ali.guliyevoo7@gmail.com>
2025-02-05 14:06:41 +0300

    stlye: add read more about me button on homepage and add underline for links

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,12 @@ useSeoMeta({
   <main class="page">
     <ContentDoc />
 
+    <NuxtLink to="/about"
+      class="w-45 flex items-center gap-x-1 mt-4 border-b border-b-primary-light hover:border-b-primary-hover border-b-solid border-b-1">
+      <span>Read more about me</span>
+      <Icon icon="mingcute:arrow-right-line" class="pt-0.5" />
+    </NuxtLink>
+
     <CurrentProjects class="mt-7" />
   </main>
 </template>
