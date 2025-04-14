ce41dcea814addcddc24538def02aed4dab79b5c
aligu7 <ali.guliyevoo7@gmail.com>
2025-04-14 17:59:33 +0300

    feat: add a custom NuxtLoadingIndicator

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,13 @@
-<!-- layouts/default.vue -->
+<script setup lang="ts">
+const isDark = useDark()
+
+const loadingColor = computed(() =>
+  isDark.value
+    ? 'repeating-linear-gradient(to right, #a1a1a1 0%, #c9c9c9 50%, #ffffff 100%)'
+    : 'repeating-linear-gradient(to right, #616161 0%, #383838 50%, #000000 100%)',
+)
+</script>
+
 <template>
   <div class="relative">
     <ClientOnly>
@@ -11,6 +20,7 @@
 
         <!-- Wrap content in a container -->
         <div class="min-h-screen">
+          <NuxtLoadingIndicator :color="loadingColor" />
           <NuxtPage />
 
           <!-- Footer after content -->
@@ -26,12 +36,12 @@
 <style lang="scss">
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.7s;
+  transition: all 0.5s;
 }
 
 .page-enter-from {
   opacity: 0;
   filter: blur(1rem);
-  transform: translateY(-20px);
+  transform: translateY(-18px);
 }
 </style>
