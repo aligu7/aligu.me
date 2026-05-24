8dd1820cc875be39f6f1002555cd72a2b29df927
Ali Guliyev <ali.guliyevoo7@gmail.com>
2026-05-24 23:16:58 +0400

    chore: add latest projects

--- components/CurrentOrLatestProjects.vue
+++ components/CurrentOrLatestProjects.vue
@@ -10,6 +10,12 @@ const currentProjects = computed(() => {
   return projects.value ? filteredCollection(projects.value, "tags", "Current Project") : []
 })
 
+const mostRecentProjects = computed(() => {
+  if (!projects.value) return []
+  return projects.value
+    .slice(0, 2)
+})
+
 // Compute grid columns based on number of projects
 const gridClass = computed(() => {
   return currentProjects.value.length === 1 ? "grid-cols-1" : "grid-cols-1 md:grid-cols-2"
@@ -17,10 +23,10 @@ const gridClass = computed(() => {
 </script>
 
 <template>
-  <div v-if="currentProjects.length > 0" class="flex flex-col">
+  <div class="flex flex-col">
     <div class="mb-3 flex items-center justify-between">
       <h1 style="margin: 0" class="text-xl">
-        Current Projects
+        {{ currentProjects.length > 0 ? 'Current Projects' : 'Recent Projects' }}
       </h1>
       <NuxtLink class="flex items-center gap-0.5" to="projects">
         <span class="text-sm md:text-base">View all projects</span>
@@ -29,10 +35,13 @@ const gridClass = computed(() => {
     </div>
 
     <ul class="grid gap-4 list-none" :class="[gridClass]">
-      <li v-for="project in currentProjects" :key="project.title" class="currentProject-item">
+      <li v-for="project in (currentProjects.length > 0 ? currentProjects : mostRecentProjects)" :key="project.title" class="currentProject-item">
         <Project :project="project" />
       </li>
     </ul>
+    <div v-if="currentProjects.length === 0 && mostRecentProjects.length === 0" class="mt-4 text-center text-gray-500 dark:text-gray-400">
+      No projects found.
+    </div>
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -7,13 +7,13 @@ useMainPageItemsRowAnimation()
 <template>
   <main class="page">
     <ContentDoc />
-    <NuxtLink
+    <!-- <NuxtLink
       to="/about"
       class="mt-4 w-45 flex items-center gap-x-1 border-b border-b-1 border-b-primary-light border-b-solid hover:border-b-primary-hover"
     >
       <span>Read more about me</span>
       <Icon icon="mingcute:arrow-right-line" class="pt-0.5" />
-    </NuxtLink>
-    <CurrentProjects class="mt-7" />
+    </NuxtLink> -->
+    <CurrentOrLatestProjects class="mt-7" />
   </main>
 </template>
