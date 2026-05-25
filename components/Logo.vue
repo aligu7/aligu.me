462f42d9bd2467bfd33105d4b4d28cfdc855bbe7
Ali Guliyev <ali.guliyevoo7@gmail.com>
2026-05-25 23:11:15 +0400

    fix: resolve incorrect logo color

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-const isDark = useDark()
 </script>
 
 <template>
@@ -8,10 +7,10 @@ const isDark = useDark()
       <path
         class="animated-path"
         d="M109.501 662.071C124.001 551.071 -18.7825 642.071 39.5008 705.071C83.9529 760.937 163.439 765.093 208.001 705.07C257.001 639.071 298.284 507.82 264.501 402.071C210.284 248.07 261.5 150.23 303 104.73M303 104.73C364 32.7296 455.284 61.5704 486.284 90.0704C561.881 159.57 439.236 270.437 394.784 214.57M303 104.73C277.732 163.432 335 327 398.618 421.73M308.284 641.57C298.784 705.07 370.984 757.127 427.784 683.57C453.196 650.662 448.433 617.947 453.5 569M453.5 569C457.664 528.773 444.653 492.5 415.5 448C386.347 403.5 407.942 436.124 398.618 421.73M453.5 569C453.5 569 519.942 555.07 562.613 507.82C605.284 460.57 642.784 460.57 642.784 460.57M398.618 421.73C462.658 343.23 516.784 363.57 577.784 301.57"
-        :stroke="isDark ? 'white' : 'black'"
+        stroke="currentColor"
         stroke-width="35"
       />
-      <circle class="animated-circle" cx="606.784" cy="696.07" r="53" :stroke="isDark ? 'white' : 'black'" stroke-width="30" />
+      <circle class="animated-circle" cx="606.784" cy="696.07" r="53" stroke="currentColor" stroke-width="30" />
     </svg>
   </NuxtLink>
 </template>
@@ -20,6 +19,11 @@ const isDark = useDark()
 svg {
   margin-top: 10px;
   margin-left: -10px;
+  color: black;
+}
+
+html.dark svg {
+  color: white;
 }
 
 .animated-path {
